718e6986f780b1a693c00e3b3bbc5e294c6d6089
amacy <aaronmacy@gmail.com>
2022-02-04 23:51:40 -0500

    day 1, part 2

--- day_01.rb
+++ day_01.rb
@@ -4,6 +4,11 @@ class Day01
     _part_1(array[0], array[1..-1], 0)
   end
 
+  def self.part_2(input)
+    array = input.split(/\n/).map(&:to_i)
+    _part_2(array[0], array[1..-1], 0)
+  end
+
   private
 
   def self._part_1(head, tail, sum)
@@ -12,6 +17,15 @@ class Day01
     else
       new_sum = tail[0] > head ? sum + 1 : sum
       _part_1(tail[0], tail[1..-1], new_sum)
+    end
+  end
+
+  def self._part_2(head, tail, sum)
+    if tail[0].nil?
+      sum
+    else
+      new_sum = tail[0..2].sum > head ? sum + 1 : sum
+      _part_2(tail[0..2].sum, tail[1..-1], new_sum)
     end
   end
 end
--- spec/day_01_spec.rb
+++ spec/day_01_spec.rb
@@ -25,4 +25,29 @@ RSpec.describe Day01 do
       expect(Day01.part_1(input)).to eq 1195
     end
   end
+
+  context "part 2" do
+    it "works for the sample input" do
+      input = <<~INPUT
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+      INPUT
+
+      expect(Day01.part_2(input)).to eq 5
+    end
+
+    it "works for the input file" do
+      input = File.read("spec/fixtures/day_01.txt")
+
+      expect(Day01.part_2(input)).to eq 1235
+    end
+  end
 end
