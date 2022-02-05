84146336652cab71198f15d59fa3dd0f3e7594ac
amacy <aaronmacy@gmail.com>
2022-02-05 00:20:49 -0500

    day 2, part 2

--- day_02.rb
+++ day_02.rb
@@ -4,6 +4,11 @@ class Day02
     _part_1(array[0], array[1..-1], 0, 0)
   end
 
+  def self.part_2(input)
+    array = input.split(/\n/)
+    _part_2(array[0], array[1..-1], 0, 0, 0)
+  end
+
   private
 
   def self._part_1(head, tail, horizontal, depth)
@@ -33,6 +38,41 @@ class Day02
           tail[1..-1],
           horizontal,
           depth - amount.to_i,
+        )
+      end
+    end
+  end
+
+  def self._part_2(head, tail, horizontal, depth, aim)
+    if head.nil?
+      horizontal * depth
+    else
+      instruction, amount = head.split(/ /)
+
+      case instruction
+      when "forward"
+        _part_2(
+          tail[0],
+          tail[1..-1],
+          horizontal + amount.to_i,
+          depth + (aim * amount.to_i),
+          aim,
+        )
+      when "down"
+        _part_2(
+          tail[0],
+          tail[1..-1],
+          horizontal,
+          depth,
+          aim + amount.to_i,
+        )
+      when "up"
+        _part_2(
+          tail[0],
+          tail[1..-1],
+          horizontal,
+          depth,
+          aim - amount.to_i,
         )
       end
     end
--- spec/day_02_spec.rb
+++ spec/day_02_spec.rb
@@ -23,5 +23,23 @@ RSpec.describe Day01 do
   end
 
   context "part 2" do
+    it "works for the sample input" do
+      input = <<~INPUT
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+      INPUT
+
+      expect(Day02.part_2(input)).to eq 900
+    end
+
+    it "works for the input file" do
+      input = File.read("spec/fixtures/day_02.txt")
+
+      expect(Day02.part_2(input)).to eq 1845455714
+    end
   end
 end
