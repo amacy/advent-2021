aa661d12f7ae6088951d85a3d3e59f1387976743
amacy <aaronmacy@gmail.com>
2022-02-12 15:03:03 -0500

    day 7, part 2

--- day_07.rb
+++ day_07.rb
@@ -5,16 +5,36 @@ class Day07
 
   def self.part_1(input)
 		parsed_input = _parse_input(input)
-    avg = parsed_input.sum / parsed_input.length
-    (0..avg).map do |candidate|
-      parsed_input.map do |crab|
-        positions = [candidate, crab].sort
-        positions.max - positions.min
-      end.sum
-    end.min
+    _part_1(parsed_input, _avg(parsed_input))
+  end
+
+  def self.part_2(input)
+		parsed_input = _parse_input(input)
+    _part_1(parsed_input, _avg(parsed_input) * 2) do |delta|
+      (1..delta).to_a.sum
+    end
   end
 
   private
+
+  def self._part_1(starting_positions, avg, &fuel_function)
+    totals = (0..avg).map do |candidate|
+      starting_positions.map do |crab|
+        positions = [candidate, crab].sort
+        delta = positions.max - positions.min
+        if block_given?
+          fuel_function.call(delta)
+        else
+          delta
+        end
+      end.sum
+    end
+      totals.min
+  end
+
+  def self._avg(parsed_input)
+    parsed_input.sum / parsed_input.length
+  end
 
   def self._parse_input(input)
     input.split(/,/).map(&:to_i)
--- spec/day_07_spec.rb
+++ spec/day_07_spec.rb
@@ -16,5 +16,16 @@ RSpec.describe Day07 do
   end
 
   context "part 2" do
+    it "works for the sample input" do
+      input = "16,1,2,0,4,2,7,1,2,14"
+
+      expect(Day07.part_2(input)).to eq 168
+    end
+
+    it "works for the input file" do
+      input = File.readlines("spec/fixtures/day_07.txt").first
+
+      expect(Day07.part_2(input)).to eq 348664
+    end
   end
 end
