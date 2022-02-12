9dd2306e566942b31aedf4868d4db55d84d18fcc
amacy <aaronmacy@gmail.com>
2022-02-12 14:03:09 -0500

    day 6

--- day_06.rb
+++ day_06.rb
@@ -3,30 +3,42 @@ require_relative "helper_methods"
 class Day06
   extend HelperMethods
 
-  def self.part_1(input)
+  def self.part_1(input, total_days)
 		parsed_input = _parse_input(input)
-    _part_1(parsed_input, 0)
+    _part_1(parsed_input, 0, total_days)
   end
 
   private
 
-  def self._part_1(lanternfish, day)
-    if day == 80
-      lanternfish.length
+  def self._part_1(lanternfish, current_day, total_days)
+    if current_day == total_days
+      lanternfish.values.sum
     else
-      new_lanternfish = lanternfish.map do |fish|
-        if fish == 0
-          [6, 8]
-        else
-          fish - 1
-        end
-      end.flatten
+      new_lanternfish = lanternfish.keys.inject({}) do |hash, days_remaining|
+        previous_count = lanternfish[days_remaining]
 
-      _part_1(new_lanternfish, day + 1)
+        if days_remaining == 0
+          hash[6] ||= 0
+          hash[6] += previous_count
+          hash[8] ||= 0
+          hash[8] += previous_count
+        else
+          hash[days_remaining - 1] ||= 0
+          hash[days_remaining - 1] += previous_count
+        end
+
+        hash
+      end
+
+      _part_1(new_lanternfish, current_day + 1, total_days)
     end
   end
 
   def self._parse_input(input)
-    input.split(/,/).map(&:to_i)
+    input.split(/,/).map(&:to_i).inject({}) do |hash, days_remaining|
+      hash[days_remaining] ||= 0
+      hash[days_remaining] += 1
+      hash
+    end
   end
 end
--- spec/day_06_spec.rb
+++ spec/day_06_spec.rb
@@ -5,16 +5,27 @@ RSpec.describe Day06 do
     it "works for the sample input" do
       input = "3,4,3,1,2"
 
-      expect(Day06.part_1(input)).to eq 5934
+      expect(Day06.part_1(input, 80)).to eq 5934
     end
 
     it "works for the input file" do
       input = File.readlines("spec/fixtures/day_06.txt").first
 
-      expect(Day06.part_1(input)).to eq 6189
+      expect(Day06.part_1(input, 80)).to eq 365862
     end
   end
 
   context "part 2" do
+    it "works for the sample input" do
+      input = "3,4,3,1,2"
+
+      expect(Day06.part_1(input, 256)).to eq 26984457539
+    end
+
+    it "works for the input file" do
+      input = File.readlines("spec/fixtures/day_06.txt").first
+
+      expect(Day06.part_1(input, 256)).to eq 1653250886439
+    end
   end
 end
